f93c31189ea06f99150bbc0b8e269af4605d286c
Nancy Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-07-18 00:04:40 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let getSuit = () => {
+  let suits = ["♠", "♣", "♥", "♦"];
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+  return suits[suitsIndex];
 };
+
+let getValue = () => {
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let valuesIndex = Math.floor(Math.random() * values.length);
+  return values[valuesIndex];
+};
+
+// console.log(getSuit);
+// console.log(getValue);
+
+window.onload = () => {
+  document.querySelector(".card").classList.add(getSuit());
+  document.querySelector(".card").classList.add(getValue());
+};
+console.log("what is happening?");
